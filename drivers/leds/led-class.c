49fae8cc194ef1c24db5c8fbddd8c2c531f3c74b
xSylla <syllamodder@gmail.com>
2021-11-13 12:42:59 +0100

    drivers: leds: Import Xiaomi changes

--- drivers/leds/led-class.c
+++ drivers/leds/led-class.c
@@ -93,8 +93,10 @@ static ssize_t max_brightness_store(struct device *dev,
 	if (ret)
 		return ret;
 
-	led_cdev->max_brightness = state;
-	led_set_brightness(led_cdev, led_cdev->usr_brightness_req);
+	if (state <= LED_FULL) {
+		led_cdev->max_brightness = state;
+		led_set_brightness(led_cdev, led_cdev->usr_brightness_req);
+	}
 
 	return size;
 }
